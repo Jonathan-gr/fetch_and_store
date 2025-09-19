85349497bcd5125a1f0ddc836aa05fdc71277813
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-09-19 18:21:46 +0300

    decided to go with fastAPI

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hllo, FastAPI is runhuning!"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello {name}"}
